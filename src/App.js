22e4a9f429bb30c9c139d451abf0caa5c6982722
Ashishkumar Gupta <adoreashish@gmail.com>
2022-10-09 18:21:08 +0530

    3.2 State and useState Hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Header from "./components/Header"
+import FeedbackItem from "./components/FeedbackItem"
 
 function App(){
     return (
         <>
             <Header />        
             <div className="container">
-                <h1>Ashish App component!</h1>
+                <FeedbackItem />
             </div>
         </>
         
